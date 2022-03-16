8d7aaef212418ca80575a8700944e7cd12d33609
ronigot <ronigotlib@gmail.com>
2022-03-16 10:58:26 +0200

    Warning signs on collision

--- Assets/scripts/CarCollision.cs
+++ Assets/scripts/CarCollision.cs
@@ -1,26 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class CarCollision : MonoBehaviour
 {
+    private float timer = 0.0f;
+    private float howLongToDisplay = 5.0f;
+    private String colText = "You have collided with the sidewalk!";
+
+    public Text text;
+    public GameObject warningCanvas;
+
+    void Start()
+    {
+        warningCanvas.SetActive(false);
+        text.text = colText;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > howLongToDisplay)
+        {
+            warningCanvas.SetActive(false);
+            timer = 0.0f;
+        }
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         //Check for a match with the specified name on any GameObject that collides with your GameObject
         if (collision.gameObject.name == "Road_1_line" || collision.gameObject.name == "Crossroads_1_lines_walk" ||
-            collision.gameObject.name == "Crossroads_1_lines_circ" || collision.gameObject.name == "Road_1_line_turn" ||
-            collision.gameObject.name == "Traffic_pot prefab")
+            collision.gameObject.name == "Crossroads_1_lines_circ" || collision.gameObject.name == "Road_1_line_turn")
         {
             //If the GameObject's name matches the one you suggest, output this message in the console
-            Debug.Log("Do something else here");
-        }
-
-        //Check for a match with the specific tag on any GameObject that collides with your GameObject
-        if (collision.gameObject.tag == "MyGameObjectTag")
-        {
-            //If the GameObject has the same tag as specified, output this message in the console
-            Debug.Log("Do something else here");
+            //Debug.Log("Do something else here");
+            warningCanvas.SetActive(true);
         }
     }
 }
