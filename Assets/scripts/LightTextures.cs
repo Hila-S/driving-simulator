f7035396a0b4e2b20c1905239ca5ba21e211b5e4
rkoolyk <rachelkoolyk@gmail.com>
2022-03-24 21:06:24 +0200

    added scene for main menu with buttons for each option

---
+++ Assets/scripts/LightTextures.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTextures : MonoBehaviour
+{
+    public Texture regularLightsTexture;
+    public Texture emptyAndRedYellowTexture;
+}
+
