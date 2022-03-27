702b53dcf1e41619fb08c3b8952167c6f979ce95
rkoolyk <rachelkoolyk@gmail.com>
2022-03-27 19:06:59 +0300

    new scene for end of game, switching between scenes, make game stop after certain distance

---
+++ Assets/scripts/Arrows.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Random;
+
+public class Arrows : MonoBehaviour
+{
+    public GameObject direction_img;
+    public Sprite up_arrow;
+    public Sprite right_arrow;
+    public Sprite left_arrow;
+    public Sprite u_turn_arrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "directions-rls")
+        {
+            int rInt = Range(0, 2); //for ints
+            if (rInt == 0)
+            {
+                direction_img.GetComponent<Image>().sprite = up_arrow;
+            }
+            if (rInt == 1)
+            {
+                direction_img.GetComponent<Image>().sprite = left_arrow;
+            }
+            if (rInt == 2)
+            {
+                direction_img.GetComponent<Image>().sprite = right_arrow;
+            }
+        }
+        if (col.gameObject.name == "directions-rlsu")
+        {
+            int rInt = Range(0, 3); //for ints
+            if (rInt == 0)
+            {
+                direction_img.GetComponent<Image>().sprite = up_arrow;
+            }
+            if (rInt == 1)
+            {
+                direction_img.GetComponent<Image>().sprite = left_arrow;
+            }
+            if (rInt == 2)
+            {
+                direction_img.GetComponent<Image>().sprite = right_arrow;
+            }
+            if (rInt == 3)
+            {
+                direction_img.GetComponent<Image>().sprite = u_turn_arrow;
+            }
+        }
+
+    }
+}
---
+++ Assets/scripts/DistanceTravelled.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DistanceTravelled : MonoBehaviour
+{
+    float distanceTravelled = 0;
+    Vector3 lastPosition;
+
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    void Update()
+    {
+        distanceTravelled += Vector3.Distance(transform.position, lastPosition) * 3.5f;
+        lastPosition = transform.position;
+        Debug.Log(distanceTravelled);
+        if (distanceTravelled > 500)
+            SceneManager.LoadScene("EndGame");
+    }
+}
