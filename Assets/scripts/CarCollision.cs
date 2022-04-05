7bf4572fa835d0837afac34714ff36d556806cb6
ronigot <ronigotlib@gmail.com>
2022-04-05 22:30:53 +0300

    Creating roads and sidewalks

--- Assets/scripts/CarCollision.cs
+++ Assets/scripts/CarCollision.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 
-public class CarCollision : WarningDisplayer
+/*public class CarCollision : WarningDisplayer
 {
     private String colText = "You have collided with the sidewalk!";
 
@@ -19,5 +19,27 @@ public class CarCollision : WarningDisplayer
             //If the GameObject's name matches the one you suggest, output this message in the console
             displayWarning(colText);
         }
+    }
+}*/
+
+public class CarCollision : MonoBehaviour
+{
+    //private String colText = "You have collided with the sidewalk!";
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == "Road_1_line" || collision.gameObject.name == "Crossroads_1_lines_walk" ||
+            collision.gameObject.name == "Crossroads_1_lines_circ" || collision.gameObject.name == "Road_1_line_turn" ||
+            collision.gameObject.name == "Sidewalk_08" || collision.gameObject.name == "Sideway 8 prefab (4)")
+        {
+            Debug.Log("collision");
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "sidewalk")
+            Debug.Log("collision");
     }
 }
